0c0d0531e7d6233d88f2a7374c9efa36ed7c0677
vanya <vanokua@gmail.com>
2013-03-15 15:12:07 +0200

    add validator form

--- js/flip-card.js
+++ js/flip-card.js
@@ -1,11 +1,13 @@
 var init = function() {
   var card = document.getElementById('card');
   
-  document.getElementById('signup').addEventListener( 'click', function(){
+  document.getElementById('signup').addEventListener( 'click', function(evt){
     card.toggleClassName('flipped');
+    evt.preventDefault();
   }, false);
-  document.getElementById('signin').addEventListener( 'click', function(){
+  document.getElementById('signin').addEventListener( 'click', function(evt){
     card.toggleClassName('flipped');
+    evt.preventDefault();
   }, false);
 
 };
--- js/myscript.js
+++ js/myscript.js
@@ -1,5 +1,60 @@
+$(document).ready(function(){   
 
-function checkform(t) {
-	console.log(t);
-	return false;
+function testN(filed) {
+	if (filed == "") return "set name.\n" ;
+	if (/[\\\/\:\"\'\#\!\%\$\%\&]/.test(filed)) return "wrong name.\n";
+	return "";
 }
+
+function testP(filed) {
+	if (filed == "") return "set password.\n" ;
+	return "";
+}
+
+function testPP(filed,filed2) {
+	if (filed != filed2) return "passwords do not match.\n" ;
+	return "";
+}
+
+function testE(filed) {
+	if (filed == "") return "set email.\n";
+	if(!(/^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,6})+$/i.test(filed))) return "wrong email.\n" ;
+
+	return "";
+}
+
+
+$('#formsignin').submit(function(form) {
+	var f = form.target;
+
+	var mesg = testE(f.email.value);
+	mesg += testP(f.pass.value);
+	
+	if (mesg == "") {
+		alert('OK');
+		form.preventDefault();	
+	}else{
+		alert(mesg);
+		form.preventDefault();	
+	}
+});
+
+$('#formsignup').submit(function(form) {
+	var f = form.target;
+
+	var mesg = testN(f.name.value);
+	mesg += testE(f.email.value);
+	mesg += testP(f.pass.value);
+	mesg += testPP(f.pass.value,f.pass2.value);
+	
+	if (mesg == "") {
+		alert('OK');
+		form.preventDefault();	
+	}else{
+		alert(mesg);
+		form.preventDefault();	
+	}
+});
+
+
+ });
