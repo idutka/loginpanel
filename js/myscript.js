dae6353e2b112095199f8bfd393d13d8bc153c5d
vanya <vanokua@gmail.com>
2013-03-18 10:38:39 +0200

    add valid/notvalid

--- js/myscript.js
+++ js/myscript.js
@@ -1,40 +1,59 @@
 $(document).ready(function(){   
 
-function testN(filed) {
-	if (filed == "") return "set name.\n" ;
-	if (/[\\\/\:\"\'\#\!\%\$\%\&]/.test(filed)) return "wrong name.\n";
+function notValid(e,title) {
+	$(e).removeClass("valid").addClass("notvalid");
+	$(e).attr("title",title);
+}
+
+function valid(e) {
+	$(e).removeClass("notvalid").addClass("valid");
+	$(e).removeAttr("title");
+}
+
+function testN(f) {
+	var v = f.value;
+	if (v == ""){ notValid(f,"set name"); return "set name.\n"; };
+	if (/[\\\/\:\"\'\#\!\%\$\%\&]/.test(v)){ notValid(f,"wrong name");  return "wrong name.\n"; };
+	valid(f);
 	return "";
 }
 
-function testP(filed) {
-	if (filed == "") return "set password.\n" ;
+function testP(f) {
+	var v = f.value;
+	if (v == ""){ notValid(f,"set password");  return "set password.\n"; };
+	valid(f);
 	return "";
 }
 
-function testPP(filed,filed2) {
-	if (filed != filed2) return "passwords do not match.\n" ;
+function testPP(f,f2) {
+	var v = f.value;
+	var v2 = f2.value;
+	if (v != v2){ notValid(f2,"passwords do not match");  return "passwords do not match.\n"; };
+	valid(f2);
 	return "";
 }
 
-function testE(filed) {
-	if (filed == "") return "set email.\n";
-	if(!(/^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,6})+$/i.test(filed))) return "wrong email.\n" ;
-
+function testE(f) {
+	var v = f.value;
+	if (v == ""){ notValid(f,"set email"); return "set email.\n"; };
+	if(!(/^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,6})+$/i.test(v))){notValid(f,"wrong email"); return "wrong email.\n"; };
+	valid(f);
 	return "";
 }
+
 
 
 $('#formsignin').submit(function(form) {
 	var f = form.target;
 
-	var mesg = testE(f.email.value);
-	mesg += testP(f.pass.value);
+	var mesg = testE(f.email);
+	mesg += testP(f.pass);
 	
 	if (mesg == "") {
 		alert('OK');
 		form.preventDefault();	
 	}else{
-		alert(mesg);
+		console.log(mesg);
 		form.preventDefault();	
 	}
 });
@@ -42,16 +61,16 @@ $('#formsignin').submit(function(form) {
 $('#formsignup').submit(function(form) {
 	var f = form.target;
 
-	var mesg = testN(f.name.value);
-	mesg += testE(f.email.value);
-	mesg += testP(f.pass.value);
-	mesg += testPP(f.pass.value,f.pass2.value);
+	var mesg = testN(f.name);
+	mesg += testE(f.email);
+	mesg += testP(f.pass);
+	mesg += testPP(f.pass,f.pass2);
 	
 	if (mesg == "") {
 		alert('OK');
 		form.preventDefault();	
 	}else{
-		alert(mesg);
+		console.log(mesg);
 		form.preventDefault();	
 	}
 });
